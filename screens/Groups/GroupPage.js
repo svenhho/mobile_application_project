57bbd3b873f0e5509d944718de15942831e8e26f
SwarnyAppakkuddy <84567450+SwarnyAppakkuddy@users.noreply.github.com>
2023-01-23 12:35:29 +0100

    change ui on login and register

--- screens/Groups/GroupPage.js
+++ screens/Groups/GroupPage.js
@@ -1,8 +1,48 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { TextInput, View, TouchableOpacity, Text, StyleSheet, KeyboardAvoidingView, Button } from 'react-native'
 
 export default function GroupPage() {
     return (
         <Text>Workout planning</Text>
     )
+
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        title: {
+            fontSize: 32,
+            marginBottom: 48,
+            color: '#ff5b5b',
+            fontWeight: 'bold',
+        },
+        input: {
+            width: '80%',
+            height: 48,
+            borderWidth: 1,
+            borderColor: '#ff5b5b',
+            borderRadius: 24,
+            paddingLeft: 16,
+            marginVertical: 8,
+            color: '#ff5b5b',
+        },
+
+        loginButton: {
+            width: '80%',
+            height: 48,
+            backgroundColor: '#ff5b5b',
+            borderRadius: 24,
+            marginVertical: 16,
+        },
+        signUp: {
+            alignSelf: 'center',
+        },
+        signUpText: {
+            color: '#ff5b5b',
+        },
+    });
 }
