d476c853b1c7954e6d5014b2d1b7dffb648bb1d5
Sven Herman Holmsen <svenhermanholmsen@gmail.com>
2023-01-28 14:33:58 +0100

    map component

---
+++ components/MapComponent.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapboxGL from '@rnmapbox/maps';
+
+MapboxGL.setAccessToken('pk.eyJ1Ijoic3ZlbmhobyIsImEiOiJjbGRlbGx2bTUwZXN6M3ZtdTB0Ymx1OTAwIn0.mKZZLjGfkeNtMXtrPNMd7g',);
+
+const MapComponent = () => {
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapboxGL.MapView style={styles.map} />
+      </View>
+    </View>
+  );
+}
+
+export default MapComponent;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: 300,
+    width: 300,
+  },
+  map: {
+    flex: 1
+  }
+});
