e74ec0a0dab6f3b1aa08048416bf179c662455b2
SwarnyAppakkuddy <84567450+SwarnyAppakkuddy@users.noreply.github.com>
2023-02-02 22:05:11 +0100

    popup med mail med group profile

--- components/MatchCard.js
+++ components/MatchCard.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
 
-const MatchCard = ({ groupData, navigation }) => (
-    <TouchableOpacity
-    >
-        <View style={styles.groupContainer}>
-            <Image
-                style={styles.groupImage}
-                source={{ uri: groupData.image }}
-            />
-            <Text style={styles.groupName}>{groupData.name}</Text>
-        </View>
-    </TouchableOpacity>
-);
+const MatchCard = ({ groupData, navigation }) => {
+    const [modalVisible, setModalVisible] = React.useState(false);
+
+    return (
+        <>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <View style={styles.groupContainer}>
+                    <Image
+                        style={styles.groupImage}
+                        source={{ uri: groupData.image }}
+                    />
+                    <Text style={styles.groupName}>{groupData.name}</Text>
+                </View>
+            </TouchableOpacity>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalInnerContainer}>
+                        <Text style={styles.modalTitle}>Group Leader</Text>
+                        <Text style={styles.modalContent}>{groupData.groupleader}</Text>
+                        <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.closeButton}>
+                            <Text style={styles.closeButtonText}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </>
+    );
+};
 
 const styles = StyleSheet.create({
     groupContainer: {
@@ -32,7 +53,42 @@ const styles = StyleSheet.create({
     },
     groupName: {
         fontWeight: 'bold'
-    }
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalInnerContainer: {
+        padding: 20,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        width: 250
+    },
+    modalTitle: {
+        fontWeight: 'bold',
+        marginBottom: 10,
+        fontWeight: '700',
+        fontSize: 20
+    },
+    modalContent: {
+        textAlign: 'center',
+        fontSize: 20
+
+    },
+    closeButton: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: '#ff5a5f',
+        borderRadius: 10,
+
+    },
+    closeButtonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16
+    },
 });
 
 export default MatchCard;
